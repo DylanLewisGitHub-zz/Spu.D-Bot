df6ce01e12cccb2d8f5b656d1f3c8376eef2aa74
Dylan Lewis <dylanthespud@gmail.com>
2021-11-13 15:50:17 +0000

    Update main.py
    Added Reaction Roles functionality

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import discord
 import os
 
-from discord.ext import commands
 from keep_alive import keep_alive
 
-client = commands.Bot(command_prefix = ".d")
+intents = discord.Intents.default()
+intents.members= True
+client = discord.Client(intents=intents)
 token = os.environ['TOKEN']
 
 @client.event
@@ -12,28 +13,52 @@ async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
 @client.event
-async def on_member_join(member):
-  print(f'{member} has joined a server.')
+async def on_raw_reaction_add(payload):
+  message_id = payload.message_id
+  if message_id == 909089626910752839:
+    guild_id = payload.guild_id
+    guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+    if payload.emoji.name == 'globe_with_meridians':
+      role = discord.utils.get(guild.roles, name='Portfolio Website Viewer')
+    elif payload.emoji.name == 'robot':
+      role = discord.utils.get(guild.roles, name='Spu.D Bot Viewer')
+    elif payload.emoji.name == 'the_potato_pack':
+      role = discord.utils.get(guild.roles, name='The Potato Pack Viewer')
+    else:
+      role = discord.utils.get(guild.roles, name=payload.emoji.name)
+    if role is not None:
+      member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+      if member is not None:
+        await member.add_roles(role)
+        print("done")
+      else:
+        print("Member not found")
+    else:
+      print("Role not found")
 
 @client.event
-async def on_member_remove(member):
-  print('{member} has left a server')
-
-@client.command()
-async def ping(ctx):
-  await ctx.send(f"Latency is {round(client.latency * 1000)}ms")
-
-@client.command()
-async def clear(ctx, amount=5):
-  await ctx.channel.purge(limit=amount+1)
-
-@client.command()
-async def kick(ctx, member : discord.Member, *, reason=None):
-  await member.kick(reason=reason)
-
-@client.command()
-async def ban(ctx, member : discord.Member, *, reason=None):
-  await member.ban(reason=reason)
+async def on_raw_reaction_remove(payload):
+  message_id = payload.message_id
+  if message_id == 909089626910752839:
+    guild_id = payload.guild_id
+    guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+    if payload.emoji.name == 'globe_with_meridians':
+      role = discord.utils.get(guild.roles, name='Portfolio Website Viewer')
+    elif payload.emoji.name == 'robot':
+      role = discord.utils.get(guild.roles, name='Spu.D Bot Viewer')
+    elif payload.emoji.name == 'the_potato_pack':
+      role = discord.utils.get(guild.roles, name='The Potato Pack Viewer')
+    else:
+      role = discord.utils.get(guild.roles, name=payload.emoji.name)
+    if role is not None:
+      member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+      if member is not None:
+        await member.remove_roles(role)
+        print("done")
+      else:
+        print("Member not found")
+    else:
+      print("Role not found")
 
 keep_alive()
 client.run(token)
